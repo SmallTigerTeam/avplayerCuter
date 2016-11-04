c495cbda05df478dc6f44860288cfbf83d3bc5c9
mac_w <mac_w@mac-wdeMac-mini.local>
2016-11-04 18:06:47 +0800

    Initial Commit

---
+++ avplayerCuter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  avplayerCuter
+//
+//  Created by mac_w on 2016/11/4.
+//  Copyright © 2016年 aee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
